0e699fddced9d5c3b940d27a8832c31a8cf665ba
alanaamerico22 <145569370+alanaamerico22@users.noreply.github.com>
2025-09-16 12:13:03 -0300

    ggg

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
-numeroSenha.textContant = tamanhoSenha;
+numeroSenha.textContent = tamanhoSenha;
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
-const simbolos = '@#?!*/&%$=+';
+const símbolos = '@#&*!?/%$=+';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 const forcaSenha = document.querySelector('.forca');
-
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
@@ -30,12 +29,11 @@ function aumentaTamanho() {
     geraSenha();
 }
 
-
 for (i = 0; i < checkbox.length; i++) {
     checkbox[i].onclick = geraSenha;
 }
 
-
+geraSenha()
 
 function geraSenha() {
     let alfabeto = '';
@@ -52,7 +50,7 @@ function geraSenha() {
     }
 
     if (checkbox[3].checked) {
-        alfabeto = alfabeto + simbolos;
+        alfabeto = alfabeto + símbolos;
     }
 
     let senha = '';
@@ -69,20 +67,19 @@ function classificaSenha() {
 
     let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
 
-
-
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (entropia > 57) {
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha > 35 && tamanhoSenha < 57) {
+    } else if (entropia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
     } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
+
     }
-
-
 
     const valorEntropia = document.querySelector('.entropia');
     valorEntropia.textContent = "Um computador pode levar até " +
-    Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+    Math.floor(2**entropia/(100e6*60*24)) + " dias para descobrir essa senha.";
+
+
 }
